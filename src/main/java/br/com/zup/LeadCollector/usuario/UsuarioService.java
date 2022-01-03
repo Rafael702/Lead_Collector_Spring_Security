a019ab04efa313369db123022a31815c4bace213
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-01-03 14:05:50 -0300

    Criar Service para usuario.

---
+++ src/main/java/br/com/zup/LeadCollector/usuario/UsuarioService.java
@@ -0,0 +1,12 @@
+package br.com.zup.LeadCollector.usuario;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UsuarioService {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+
+}
