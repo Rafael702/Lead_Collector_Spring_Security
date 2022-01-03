a47fa018c214aca1a70010e74d7d7635fd2ea538
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-01-03 14:06:08 -0300

    Criar Controller para usuario.

---
+++ src/main/java/br/com/zup/LeadCollector/usuario/UsuarioController.java
@@ -0,0 +1,11 @@
+package br.com.zup.LeadCollector.usuario;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+
+
+}
