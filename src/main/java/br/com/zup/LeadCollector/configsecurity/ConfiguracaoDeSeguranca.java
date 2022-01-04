56f13d1da403724f8df0e689924dcf4389bfa94a
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-01-04 11:20:36 -0300

    Sobrescrever método configure do Spring Security.

---
+++ src/main/java/br/com/zup/LeadCollector/configsecurity/ConfiguracaoDeSeguranca.java
@@ -0,0 +1,22 @@
+package br.com.zup.LeadCollector.configsecurity;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
+    private static final String[] ENDPOINT_POST_PUBLICO = {
+            "/leads",
+            "/usuario"
+    };
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers(HttpMethod.POST, ENDPOINT_POST_PUBLICO).permitAll()
+                .anyRequest().authenticated().and().csrf().disable();
+    }
+}
