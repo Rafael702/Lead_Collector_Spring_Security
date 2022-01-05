ef7e96796aa169163fb3e2443810227d552ed1fb
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-01-05 06:49:03 -0300

    Retirar retorno do método.

--- src/main/java/br/com/zup/LeadCollector/usuario/UsuarioController.java
+++ src/main/java/br/com/zup/LeadCollector/usuario/UsuarioController.java
@@ -3,7 +3,6 @@ package br.com.zup.LeadCollector.usuario;
 import br.com.zup.LeadCollector.usuario.dtos.CadastrarUsuarioDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,10 +14,10 @@ public class UsuarioController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Usuario cadastrarUsuario(@RequestBody CadastrarUsuarioDTO cadastrarUsuarioDTO) {
+    public void cadastrarUsuario(@RequestBody CadastrarUsuarioDTO cadastrarUsuarioDTO) {
         Usuario usuario = new Usuario();
         usuario.setEmail(cadastrarUsuarioDTO.getEmail());
         usuario.setSenha(cadastrarUsuarioDTO.getSenha());
-        return usuarioService.salvarUsuario(usuario);
+        usuarioService.salvarUsuario(usuario);
     }
 }
