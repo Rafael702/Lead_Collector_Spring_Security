5efbad02d7dd51fbaa61a8d89e912a4f2adc1432
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-01-11 07:24:26 -0300

    Criar JWTComponente para gerar, validar token e pegar claim.

--- src/main/java/br/com/zup/LeadCollector/config/security/JWT/JWTComponent.java
+++ src/main/java/br/com/zup/LeadCollector/config/security/JWT/JWTComponent.java
@@ -1,5 +1,7 @@
 package br.com.zup.LeadCollector.config.security.JWT;
 
+import br.com.zup.LeadCollector.config.security.exception.TokenInvalidoException;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Component;
@@ -19,5 +21,31 @@ public class JWTComponent {
                 .signWith(SignatureAlgorithm.HS512, segredo.getBytes()).compact();
 
         return token;
+    }
+
+    public Claims pegarClaims(String token) {
+        try {
+            Claims claims = Jwts.parser().setSigningKey(segredo.getBytes())
+                    .parseClaimsJws(token).getBody();
+            return claims;
+        } catch (Exception exception) {
+            throw new TokenInvalidoException();
+        }
+    }
+
+    public boolean tokenValido(String token) {
+        try {
+            Claims claims = pegarClaims(token);
+
+            String username = claims.getSubject();
+            Date vencimentoToken = claims.getExpiration();
+            if (username != null && vencimentoToken != null) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (TokenInvalidoException tokenInvalido) {
+            return false;
+        }
     }
 }
